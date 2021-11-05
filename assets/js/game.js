5ae112e01de2aa9dd615c8a685b21d2dd42dc3af
Cadenza <mirandalucsb@gmail.com>
2021-11-05 14:14:17 -0700

    first simple javaScript program

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+function fight(){
+    window.alert("The fight has begun!");
+}
+console.log("leave yourself a message!");
+console.log(10+10);
+console.log(playerName);
+//fight();

e3d8d94ac6485ca2d53ceca763fdbf874d97acf5
Cadenza <mirandalucsb@gmail.com>
2021-11-09 15:15:29 -0700

    randomized fight turn order

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,41 +73,65 @@ var fightOrSkip = function(){
   return false;
 }
 
-var fight = function fight(enemyInfo){
-
-    while(playerInfo.playerHealth > 0 && enemyInfo.health > 0){
-        // ask player if they'd like to fight or skip using fightOrSkip function
-        if (fightOrSkip()) {
-            // if true, leave fight by breaking loop
-            break;
-        }
-
-        var damage = randomNum(playerInfo.playerAttack-3, playerInfo.playerAttack);
-        enemyInfo.health = Math.max(0, enemyInfo.health-damage);
-        console.log(playerInfo.playerName+" attacked " + enemyInfo.name + ". " + enemyInfo.name + " now has " + enemyInfo.health + " remaining.");
-        
-        if(enemyInfo.health <= 0){
-            window.alert(enemyInfo.name + " has died!");
-            break;
-        }
-        else{
-            window.alert(enemyInfo.name + " still has " + enemyInfo.health + " hit points left.");
-        }
-        
-        var damage = randomNum(enemyInfo.attack-3, enemyInfo.attack);
-        playerInfo.playerHealth = Math.max(0, playerInfo.playerHealth-damage);
-        console.log(enemyInfo.name+" attacked " + playerInfo.playerName + ". " + playerInfo.playerName + " now has " + playerInfo.playerHealth + " remaining.");
-        
-        if(playerInfo.playerHealth <= 0){
-            window.alert(playerInfo.playerName + " has died!");
-            break;
-        }
-        else{
-            window.alert(playerInfo.playerName + " still has " + playerInfo.playerHealth + " hit points left.");
-        }
+var fight = function(enemy) {
+    // keep track of who goes first
+    var isPlayerTurn = true;
+  
+    // randomly change turn order
+    if (Math.random() > 0.5) {
+      isPlayerTurn = false;
     }
-
-}
+    while (playerInfo.health > 0 && enemy.health > 0) {
+        if (isPlayerTurn) {
+            
+            // ask player if they'd like to fight or skip using fightOrSkip function
+            if (fightOrSkip()) {
+            // if true, leave fight by breaking loop
+                break;
+            }
+  
+            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+  
+            // remove enemy's health by subtracting the amount we set in the damage variable
+            enemy.health = Math.max(0, enemy.health - damage);
+            console.log(playerInfo.name +" attacked " +enemy.name +". " +enemy.name +" now has " +enemy.health +" health remaining.");
+  
+            // check enemy's health
+            if (enemy.health <= 0) {
+                window.alert(enemy.name + " has died!");
+  
+                // award player money for winning
+                playerInfo.money = playerInfo.money + 20;
+  
+                // leave while() loop since enemy is dead
+                break;
+            } 
+            else {
+            window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
+        // player gets attacked first
+        } 
+        else {
+            var damage = randomNumber(enemy.attack - 3, enemy.attack);
+  
+            // remove player's health by subtracting the amount we set in the damage variable
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+            console.log(enemy.name +" attacked " +playerInfo.name +". " +playerInfo.name +" now has " +playerInfo.health +" health remaining.");
+  
+            // check player's health
+            if (playerInfo.health <= 0) {
+                window.alert(playerInfo.name + " has died!");
+                // leave while() loop if player is dead
+                break;
+            } 
+            else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            }
+        }
+        // switch turn order for next round
+        isPlayerTurn = !isPlayerTurn;
+    }
+  };
 
 var startGame = function(){
     playerInfo.reset();
